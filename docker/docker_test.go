1b62bdfb2f59915375c9bdace9c6f1b2050cf8a7
EvanSnapp <evan.snapp@leankit.com>
2016-10-19 16:53:27 -0500

    reformating docker test

--- docker/docker_test.go
+++ docker/docker_test.go
@@ -8,9 +8,11 @@ import (
 
 func TestHookImage(t *testing.T) {
 	g := goblin.Goblin(t)
-	g.Describe("Docker Command Creation", func() {
-		g.It("Command should have correct arguments", func() {
-			g.Assert(CreateCmd([]string{"abc", "efg"}, true).Args).Equal([]string{DockerCmd, "abc", "efg"})
+	g.Describe("Docker", func() {
+		g.Describe("Command Creation", func() {
+			g.It("should have correct arguments", func() {
+				g.Assert(CreateCmd([]string{"abc", "efg"}, true).Args).Equal([]string{DockerCmd, "abc", "efg"})
+			})
 		})
 	})
 }
